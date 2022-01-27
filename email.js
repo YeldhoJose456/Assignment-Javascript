05a3743676dd0bfb7c6786327b64aad0036399bf
Eldho Jose <98265870+YeldhoJose456@users.noreply.github.com>
2022-01-27 19:54:53 +0530

    Day 2 Assignment submitted

---
+++ email.js
@@ -0,0 +1,11 @@
+const email = prompt('Please enter your email')
+const password = prompt('Please enter your password')
+
+if(email === 'stu@letsupgrade.com' && password === '12345')
+{
+    alert('you are logged in')
+}
+else
+{
+    alert('try again with correct email and password')
+}
